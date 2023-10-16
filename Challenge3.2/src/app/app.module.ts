c6e39e4226f9756a26a33c2db730289bea07f565
LAURA_MAYA DD <mayadray1@gmail.com>
2023-10-16 15:39:01 +0200

    added 2 new components

--- Challenge3.2/src/app/app.module.ts
+++ Challenge3.2/src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { MoviesComponent } from './movies/movies.component';
 import { HighlightDirective } from './highlight.directive';
 import { DisplayDirective } from './display.directive';
+import { MenuComponent } from './menu/menu.component';
+import { SearchMovieComponent } from './search-movie/search-movie.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MoviesComponent,
     HighlightDirective,
-    DisplayDirective
+    DisplayDirective,
+    MenuComponent,
+    SearchMovieComponent
   ],
   imports: [
     BrowserModule,
